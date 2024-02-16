71ad92adac10d542238b0235054849b8a183b4da
youngsun <sunnyskylove22@gmail.com>
2024-02-16 14:28:24 +0900

    [24.02.16] 클래스 객체

---
+++ src/main/java/com/ohgiraffers/section2/encapsulation/problem1/Application01.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.section2.encapsulation.problem1;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. 필드에 직접 접근하는 경우 발생할 수 있는 문제점(1) */
+        /* 필기. 필드에 올바르지 않는 값이 들어가도 통제가 불가능하다. */
+
+        // 1번 몬스터 생성
+        Monster monster1 = new Monster();
+        monster1.name = "드라큘라";
+        monster1.hp = 200;
+
+        System.out.println("monster1의 name : " + monster1.name);
+        System.out.println("monster1의 hp : " + monster1.hp);
+
+        // 2번 몬스터 생성
+        Monster monster2 = new Monster();
+        monster2.name = "헐크";
+        monster2.hp = -200;
+        // -도 입력 가능!(검증되지 않는 이상한 값을 넣어도 필드에 넣을 수 있다.)
+        // 따라서 이걸 0으로 처리되게 할 수 있음(원본인 Monster 클래스에서)
+
+        System.out.println("monster2의 name : " + monster2.name);
+        System.out.println("monster2의 hp :" + monster2.hp);
+
+        // 3번 몬스터 생성
+        Monster monster3 = new Monster();
+        monster3.name = "프랑켄슈타인";
+        monster3.setHp(200);
+        // 메소드에 직접적으로 접근하지 않고도~~?? (hp가 저장되어있는 것임)
+        // Monster 클래스에 조건(0이하이면 안됨)을 만들어줘서,
+        // 만약에 200이 아닌 -로 하면, 초기화값 0으로 변경 출력됨을 알 수 있다.
+
+        System.out.println("monster3의 name : " + monster3.name);
+        System.out.println("monster3의 hp : " + monster3.hp);
+
+
+    }
+
+}
---
+++ src/main/java/com/ohgiraffers/section2/encapsulation/problem1/Monster.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.section2.encapsulation.problem1;
+
+public class Monster {
+
+    String name;   // 몬스터 이름
+    int hp;        // 몬스터 체력
+
+    public  void setHp(int hp){
+
+        if(hp > 0){            //int 체력으로 했으니깐 0이하일때만 처리되게 만들기
+
+            /* 필기.
+            *   this 는 인스턴스가 생성되었을 때 자신의 주소를 저장하는 레퍼런스 변수이다.
+            *   지역변수와 전역변수의 이름이 동일한 경우 지역변수를 우선 접근하기 때문에
+            *   전역변수에 접근하기 위해서 this. 을 명시해준다.
+            * */
+            System.out.println("양수의 값이 입력되어서 정상적으로 변경되었습니다. ");
+            this.hp = hp;
+            // this 로 인해서 위의 hp와 같다고(int.hp = 작성하는 매개변수) 만들어준것임
+            // 그 전엔 다른 hp임 / 해당 변수 클릭시, 같은색깔과 동시 지정된 것으로도 확인 가능
+            // 메소드를 통해서 필드에 간접적으로 접근한다.
+
+        } else {
+
+            System.out.println("0보다 작거나 같은 값이 몬스터의 체력을 0으로 변경합니다.");
+            this.hp = 0;
+
+        }
+
+    }
+
+}
